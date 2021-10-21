e73ff669bcdd21fec754d631fee71a2f703090d1
NikolayCarrasco <nikolay.carrasco.v@gmail.com>
2021-10-21 10:41:35 -0300

    schema collaborator updated

--- src/app/components/collaborator/collaborator.schema.ts
+++ src/app/components/collaborator/collaborator.schema.ts
@@ -1,20 +1,13 @@
 import { Schema, model, Document } from "mongoose";
-import { User } from "../../models/user.model";
+import { Collaborator } from '../../models/collaborator.model';
 
-const definition: Partial<Record<keyof User, any>> = {
+const definition: Partial<Record<keyof Collaborator, any>> = {
   
   name: { type: String, required: true, lowercase: true, trim: true },
   lastName: { type: String, required: true, lowercase: true, trim: true },
-  movilPhone: { type: String, required: false, trim: true },
-  idDisciplines: [{ type: Schema.Types.ObjectId, required: false, ref: 'Discipline', trim: true, autopopulate: true }],
-  idSkills: [{ type: Schema.Types.ObjectId, required: false, ref: 'Skill', trim: true, autopopulate: true }],
-  idSavedProjects: [{ type: Schema.Types.ObjectId, required: false, ref: 'Project', trim: true, autopopulate: true }],
-  idMyProjects: [{ type: Schema.Types.ObjectId, required: false, ref: 'Project', trim: true, autopopulate: true }],
-  idRequestC: [{ type: Schema.Types.ObjectId, required: false, ref: 'RequestC', trim: true, autopopulate: true }],
-  idRequestResults: [{ type: Schema.Types.ObjectId, required: false, ref: 'RequestC', trim: true, autopopulate: true }],
+  idUser: { type: String, required: true}
 };
 
-const schema: Schema<User> = new Schema(definition, { timestamps: true });
-schema.plugin(require('mongoose-autopopulate'));
+const schema: Schema<Collaborator> = new Schema(definition, { timestamps: true });
 
-export default model<User & Document>('User', schema, 'user');
+export default model<Collaborator & Document>('Collaborator', schema, 'collaborator');
