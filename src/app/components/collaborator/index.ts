49d7580b6e5bd16861843170ea4ece26e7dd10bf
Anibal <anibal78e@gmail.com>
2021-10-23 17:07:56 -0300

    errores

--- src/app/components/collaborator/index.ts
+++ src/app/components/collaborator/index.ts
@@ -2,10 +2,6 @@ import express, { Express } from "express";
 import router from "./collaborator.network"
 
 const collaborator: Express = express();
-<<<<<<< HEAD
 collaborator.use('/collaborator', router);
-=======
-collaborator.use('/collaboratorr', router);
->>>>>>> fbdbd0241e4e31af54a40c6d772bc6c4dee73fcf
 
 export default collaborator;
